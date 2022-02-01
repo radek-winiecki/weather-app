350d35bef648e5c38ecf63b94fc5b8ce21c08103
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2022-02-01 12:22:45 +0100

    add some CSS styles for the search box and bar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-box" placeholder="Search...">
+        <input type="text" class="search-bar" placeholder="Search...">
       </div>
     </main>
   </div>
@@ -45,4 +45,32 @@ main {
 
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
 </style>
